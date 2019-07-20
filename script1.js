88c6314773430f4c511bcedfd2c09df0509e14c4
popsoy <pepitojamesryan01@gmail.com>
2019-07-20 14:44:48 +0800

    First Commit

---
+++ script1.js
@@ -0,0 +1,171 @@
+let products =[
+    {
+        pname : "OH/FH00/N ",
+        price : "$USD 329.00",
+        pcode : "2",
+        description: "2-years warranty on connected parts",
+        manufacturer: "DXRACER",
+        stocks : "5",
+        supplier : "James",
+        url:"https://www.dxracer.com/us/en-us/product/1/gaming-chairs/formula-and-racing-series/oh-fh00-n/",
+        imgurl:"https://d347qe3jx1i9dl.cloudfront.net/item/394ca4a2-c10a-40b5-a4ae-ac3803b102a4/title/956.jpg",
+
+    },
+    {
+        pname : "OH/FH00/N ",
+        price : "$USD 329.00",
+        pcode : "2",
+        description: "2-years warranty on connected parts",
+        manufacturer: "DXRACER",
+        stocks : "5",
+        supplier : "James",
+        url:"https://www.dxracer.com/us/en-us/product/1/gaming-chairs/formula-and-racing-series/oh-fh00-n/",
+        imgurl:"https://d347qe3jx1i9dl.cloudfront.net/item/394ca4a2-c10a-40b5-a4ae-ac3803b102a4/title/956.jpg",
+
+    },
+    {
+        pname : "OH/FH00/N ",
+        price : "$USD 329.00",
+        pcode : "2",
+        description: "2-years warranty on connected parts",
+        manufacturer: "DXRACER",
+        stocks : "5",
+        supplier : "James",
+        url:"https://www.dxracer.com/us/en-us/product/1/gaming-chairs/formula-and-racing-series/oh-fh00-n/",
+        imgurl:"https://d347qe3jx1i9dl.cloudfront.net/item/394ca4a2-c10a-40b5-a4ae-ac3803b102a4/title/956.jpg",
+
+    },
+    {
+        pname : "OH/FH00/N ",
+        price : "$USD 329.00",
+        pcode : "2",
+        description: "2-years warranty on connected parts",
+        manufacturer: "DXRACER",
+        stocks : "5",
+        supplier : "James",
+        url:"https://www.dxracer.com/us/en-us/product/1/gaming-chairs/formula-and-racing-series/oh-fh00-n/",
+        imgurl:"https://d347qe3jx1i9dl.cloudfront.net/item/394ca4a2-c10a-40b5-a4ae-ac3803b102a4/title/956.jpg",
+
+    },
+    {
+        pname : "OH/FH00/N ",
+        price : "$USD 329.00",
+        pcode : "2",
+        description: "2-years warranty on connected parts",
+        manufacturer: "DXRACER",
+        stocks : "5",
+        supplier : "James",
+        url:"https://www.dxracer.com/us/en-us/product/1/gaming-chairs/formula-and-racing-series/oh-fh00-n/",
+        imgurl:"https://d347qe3jx1i9dl.cloudfront.net/item/394ca4a2-c10a-40b5-a4ae-ac3803b102a4/title/956.jpg",
+
+    },
+    {
+        pname : "OH/FH00/N ",
+        price : "$USD 329.00",
+        pcode : "2",
+        description: "2-years warranty on connected parts",
+        manufacturer: "DXRACER",
+        stocks : "5",
+        supplier : "James",
+        url:"https://www.dxracer.com/us/en-us/product/1/gaming-chairs/formula-and-racing-series/oh-fh00-n/",
+        imgurl:"https://d347qe3jx1i9dl.cloudfront.net/item/394ca4a2-c10a-40b5-a4ae-ac3803b102a4/title/956.jpg",
+
+    },
+    {
+        pname : "OH/FH00/N ",
+        price : "$USD 329.00",
+        pcode : "2",
+        description: "2-years warranty on connected parts",
+        manufacturer: "DXRACER",
+        stocks : "5",
+        supplier : "James",
+        url:"https://www.dxracer.com/us/en-us/product/1/gaming-chairs/formula-and-racing-series/oh-fh00-n/",
+        imgurl:"https://d347qe3jx1i9dl.cloudfront.net/item/394ca4a2-c10a-40b5-a4ae-ac3803b102a4/title/956.jpg",
+
+    },
+    {
+        pname : "OH/FH00/N ",
+        price : "$USD 329.00",
+        pcode : "2",
+        description: "2-years warranty on connected parts",
+        manufacturer: "DXRACER",
+        stocks : "5",
+        supplier : "James",
+        url:"https://www.dxracer.com/us/en-us/product/1/gaming-chairs/formula-and-racing-series/oh-fh00-n/",
+        imgurl:"https://d347qe3jx1i9dl.cloudfront.net/item/394ca4a2-c10a-40b5-a4ae-ac3803b102a4/title/956.jpg",
+
+    },
+    {
+        pname : "OH/FH00/N ",
+        price : "$USD 329.00",
+        pcode : "2",
+        description: "2-years warranty on connected parts",
+        manufacturer: "DXRACER",
+        stocks : "5",
+        supplier : "James",
+        url:"https://www.dxracer.com/us/en-us/product/1/gaming-chairs/formula-and-racing-series/oh-fh00-n/",
+        imgurl:"https://d347qe3jx1i9dl.cloudfront.net/item/394ca4a2-c10a-40b5-a4ae-ac3803b102a4/title/956.jpg",
+
+    },
+    {
+        pname : "OH/FH00/N ",
+        price : "$USD 329.00",
+        pcode : "2",
+        description: "2-years warranty on connected parts",
+        manufacturer: "DXRACER",
+        stocks : "5",
+        supplier : "James",
+        url:"https://www.dxracer.com/us/en-us/product/1/gaming-chairs/formula-and-racing-series/oh-fh00-n/",
+        imgurl:"https://d347qe3jx1i9dl.cloudfront.net/item/394ca4a2-c10a-40b5-a4ae-ac3803b102a4/title/956.jpg",
+
+    },
+    {
+        pname : "OH/FH00/N ",
+        price : "$USD 329.00",
+        pcode : "2",
+        description: "2-years warranty on connected parts",
+        manufacturer: "DXRACER",
+        stocks : "5",
+        supplier : "James",
+        url:"https://www.dxracer.com/us/en-us/product/1/gaming-chairs/formula-and-racing-series/oh-fh00-n/",
+        imgurl:"https://d347qe3jx1i9dl.cloudfront.net/item/394ca4a2-c10a-40b5-a4ae-ac3803b102a4/title/956.jpg",
+
+    },
+    {
+        pname : "OH/FH00/N ",
+        price : "$USD 329.00",
+        pcode : "2",
+        description: "2-years warranty on connected parts",
+        manufacturer: "DXRACER",
+        stocks : "5",
+        supplier : "James",
+        url:"https://www.dxracer.com/us/en-us/product/1/gaming-chairs/formula-and-racing-series/oh-fh00-n/",
+        imgurl:"https://d347qe3jx1i9dl.cloudfront.net/item/394ca4a2-c10a-40b5-a4ae-ac3803b102a4/title/956.jpg",
+
+    }
+
+
+
+]
+
+let output ="";
+products.forEach(function(item){
+    if(item.imgurl == undefined)
+    {
+        item.imgurl = "https://via.placeholder.com/300"
+    }
+    output += ` <div class= 'grid-item'>
+    
+    <a target="_blank" href ="${item.url}"> <img src= ${item.imgurl}></a>
+    <a target="_blank" href ="${item.url}"><h1> ${item.pname}</h1></a>
+    <p><span>Price: </span>${item.price}</p>
+    <p><span>Code: </span>${item.pcode}</p>
+    <p><span>Description: </span>${item.description}</p>
+    <p><span>Manufacture: </span>${item.manufacturer}</p>
+    <p><span>Stocks: </span>${item.stocks}</p>
+    <p><span>Supplier: </span>${item.supplier}</p>
+    </div>
+ `
+})
+
+document.getElementById('displayProducts').innerHTML = output
